cacc105e22c108d5d2c7fb3e05a424d67c34106e
Domantas Saltenis <d.saltenis1@gmail.com>
2024-03-15 19:16:24 +0000

    Implemented flant5, uie as well as bulk annotate

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
-from .llm.llm import annotate_text, chat_with_llm
+
+from flaskr.demo_model.run_uie_inference import run_uie_inference
+from .llm.llm import annotate_text, annotate_bulk
+from .demo_model.run_flant5_inference import run_flant5_inference
+from .demo_model.format_transfer.sel2record_phee import convert_to_record
 
 app = Flask(__name__)
 cors = CORS(app)    
@@ -19,12 +23,40 @@ def annotate():
 
     return annotate_text(data["text"])
 
-@app.route("/chat", methods=["POST"])
-def chat():
-    print(request.get_json())
+@app.route("/flant5", methods=["POST"])
+def flant5():
     data = request.get_json()
+    print(data)
+
+    seq_result = run_flant5_inference(data["text"])
+    record_result = convert_to_record(seq_result)
     
-    return chat_with_llm(data["text"])
+    return record_result
+
+@app.route("/uie", methods=["POST"])
+def uie():
+    data = request.get_json()
+    print(data)
+
+    seq_result = run_uie_inference(data["text"])
+    record_result = convert_to_record(seq_result)
+    
+    return record_result
+
+@app.route("/mistral7b", methods=["POST"])
+def mistral7b():
+    data = request.get_json()
+    print(data)
+
+    return annotate_bulk(data["text"])
+
+# @app.route("/chat", methods=["POST"])
+# def chat():
+#     print(request.get_json())
+#     data = request.get_json()
+    
+#     return chat_with_llm(data["text"])
+
 
 
 if __name__ == "__main__":
--- flaskr/llm/llm.py
+++ flaskr/llm/llm.py
@@ -1,3 +1,4 @@
+from typing import List
 import untruncate_json
 import os
 from pathlib import Path
@@ -82,6 +83,65 @@ def annotate_text(query : str): # Possibly stream this too?
     return result
 
 
+def annotate_bulk(queries: List[str]):
+    llm = load_llm()    # in practice, the llm would be loaded on startup and kept in memory
+    print("LLM loaded")
+    
+    # load grammar
+    with open(PATH / "grammar.gbnf", "r") as f:
+        grammar_text = f.read()
+        grammar = LlamaGrammar.from_string(grammar_text)
+    
+    with open(PATH / "llm_additional_system_prompt_info.txt", "r", encoding="utf-8") as f:
+        extra_info = f.read()
+
+    system_message = f"""
+    You are a helpful AI that can extract information from electronic health records.
+    Given an electronic health record, extract the relevant information to produce a JSON with the following fields:
+    {extra_info}
+    Event type must be either of Adverse_event or Potential_therapeutic_effect.
+    If a annotaion field is not present, don't include it in the JSON.
+    Ensure the JSON is valid and contains the correct data types.
+    """
+    
+    results = []
+    
+    for id, query in enumerate(queries, start=1000):
+        #Annotate the text
+        output = llm.create_chat_completion(
+            messages=[
+                {
+                    "role" : "user",
+                    "content" : system_message + '7978578_1 Response of a promethazine-induced coma to flumazenil.'
+                },
+                {
+                    "role" : "assistant",
+                    "content" : '{"id":"7978578_1","context":"Response of a promethazine-induced coma to flumazenil.","events":[{"type":"Adverse_event","annotations":[{"annotation":"Trigger","text":"induced"},{"annotation":"Treatment","text":"promethazine"},{"annotation":"Treatment.Drug","text":"promethazine"},{annotation":"Effect","text":"coma"}]},{"type":"Potential_therapeutic_event","annotations":[{"annotation":"Trigger","text":"Response"},{"annotation":"Treatment","text":"flumazenil"},{"annotation":"Treatment.Disorder","text":"promethazine-induced coma"},{"annotation":"Treatment.Drug","text":"flumazenil"}]}]}',
+                },
+                {
+                    "role" : "user",
+                    "content" : '16728538_2 Drug-induced hepatitis in an acromegalic patient during combined treatment with pegvisomant and octreotide long-acting repeatable attributed to the use of pegvisomant.'
+                },
+                {
+                    "role" : "assistant",
+                    "content" : '{"id":"16728538_2","context":"Drug-induced hepatitis in an acromegalic patient during combined treatment with pegvisomant and octreotide long-acting repeatable attributed to the use of pegvisomant.","events":[{"type":"Adverse_event","annotations":[{"annotation":"Trigger","text":"induced"},{"annotation":"Effect","text":"hepatitis"},{"annotation":"Treatment","text":"combined treatment with pegvisomant and octreotide long-acting repeatable"},{"annotation":"Treatment.Disorder","text":"acromegalic"},{"annotation":"Treatment.Drug","text":"pegvisomant"},{"annotation":"Treatment.Drug","text":"octreotide"},{"annotation":"Treatment.Combination.Trigger","text":"and"},{"annotation":"Treatment.Combination.Drug","text":"pegvisomant"},{"annotation":"Treatment.Combination.Drug","text":"octreotide"},{"annotation":"Subject","text":"an acromegalic patient"},{"annotation":"Subject.Disorder","text":"acromegalic"}]}]}'
+                },
+                {
+                    "role" : "user",
+                    "content" : f"{id} {query}"
+                },
+            ],
+            max_tokens=4096,
+            temperature=0.5,
+            stop=["</s>"],
+            grammar=grammar # Format outputs with a grammar
+        )
+        output_formatted = output["choices"][0]["message"]["content"].replace("\n", "").replace('\\', '')
+        output_formatted = untruncate_json.complete(output_formatted)
+        results.append(output_formatted)
+
+    return results
+
 def chat_with_llm(prompt):
     llm = load_llm()
     
@@ -97,24 +157,3 @@ def chat_with_llm(prompt):
         stop=["</s>"],
     )["choices"][0]["message"]["content"]
     
-    # Streaming version
-    # output = llm.create_chat_completion(
-    #     messages=[
-    #         {
-    #             "role" : "user",
-    #             "content" : prompt
-    #         },
-    #     ],
-    #     max_tokens=4096,
-    #     temperature=0.5,
-    #     stop=["</s>"],
-    #     stream=True,
-    # )
-    # for chunk in output: # https://github.com/abetlen/llama-cpp-python/discussions/319
-    #     delta = chunk['choices'][0]['delta']
-    #     if 'role' in delta:
-    #         print(delta['role'], end=': ')
-    #     elif 'content' in delta:
-    #         print(delta['content'], end='')
-            
-    # figure out how to stream the response back to the user
